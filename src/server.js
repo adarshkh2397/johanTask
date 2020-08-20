ca0d3ee14282860bb1695e0869c09244e9ed198c
SinOfPride07 <tusharkhandelwal.work@gmail.com>
2020-08-20 15:37:52 +0530

    3

--- src/server.js
+++ src/server.js
@@ -42,15 +42,15 @@ const path = require("path");
     app.use('/api', apiRouter);
     apiRouter.use('/users', userRoutes);
     apiRouter.use('/session', sessionRoutes);
-
+  
+    if (process.env.NODE_ENV === 'production') {
 
     app.use(express.static(path.join(__dirname,"../frontend/build")));
-
     app.get("*", (req, res) => {
         res.sendFile(
           path.join(__dirname+"../frontend/build/index.html")
         );
-      });
+      });}
     app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
   } catch (err) {
     console.log(err);
